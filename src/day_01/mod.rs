43809b116c1c87d692699c662876772e27ebe1b8
Justin Vaughn <gearphreak@gmail.com>
2022-11-30 22:04:00 -0800

    Completed AoC_2022 Day 1 Part 1 & 2

---
+++ src/day_01/mod.rs
@@ -0,0 +1,56 @@
+pub mod solutions {
+
+  // Day 1 Part 1 -- https://adventofcode.com/2022/day/1
+  pub fn find_elf_carrying_most_calories(elf_food_logs: &Vec<&str>) {
+    let mut i: usize = 0;
+    let mut current_elf_calories: u32 = 0;
+    let mut most_calories_so_far: u32 = 0;
+
+    while i < elf_food_logs.len() {
+      // Set up for new elf if the line is empty
+      if elf_food_logs[i].len() == 0 {
+        if current_elf_calories > most_calories_so_far {
+          most_calories_so_far = current_elf_calories;
+        }
+        current_elf_calories = 0;
+      } else {
+        let item_calories: u32 = elf_food_logs[i].parse::<u32>().unwrap();
+        current_elf_calories += item_calories;
+      }
+      i += 1;
+    }
+
+    println!("Highest Calorie Count: {most_calories_so_far}");
+  }
+
+  // Day 1 Part 2 -- https://adventofcode.com/2022/day/1#part2
+  pub fn find_top_three_calorie_sum(elf_food_logs: &Vec<&str>) {
+    let mut i: usize = 0;
+    let mut current_elf_calories: u32 = 0;
+    let mut most_calories_so_far: Vec<u32> = vec![0, 0, 0]; // (highest -> lowest)
+
+    while i < elf_food_logs.len() {
+      // Set up for new elf if the line is empty
+      if elf_food_logs[i].len() == 0 {
+        if current_elf_calories > most_calories_so_far[2] {
+          most_calories_so_far.push(current_elf_calories);
+          most_calories_so_far.sort_by(|a,b| b.cmp(a));
+          if most_calories_so_far.len() > 3 {
+            most_calories_so_far.pop();
+          }
+        }
+        current_elf_calories = 0;
+      } 
+      // Otherwise just thicc en up the current elf
+      else {
+        let item_calories: u32 = elf_food_logs[i].parse::<u32>().unwrap();
+        current_elf_calories += item_calories;
+      }
+
+      i += 1;
+    }
+
+    println!("Highest calorie elves: {most_calories_so_far:?}");
+    println!("Calorie Count Sum: {}", most_calories_so_far.iter().sum::<u32>());
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use std::{fs};
 
+mod day_01;
+
 fn main() {
-    let contents = fs::read_to_string("./src/input.txt")
+    let contents = fs::read_to_string("./src/day_01/input.txt")
       .expect("Should have been able to read the file");
 
-    let lines: Vec<&str> = contents
+    let puzzle_input: Vec<&str> = contents
       .split(['\n'])
       .map(|e| e.trim())
       .collect();
 
-    println!("{lines:?}")
+    // day_01::solutions::find_elf_carrying_most_calories(&puzzle_input);
+    day_01::solutions::find_top_three_calorie_sum(&puzzle_input);
 }
