75c917e0c0e758cef45f03ea78bce336d3f85da3
Justin Vaughn <gearphreak@gmail.com>
2022-12-06 18:05:30 -0700

    Day 5 Part 1 completed

--- src/day_05/mod.rs
+++ src/day_05/mod.rs
@@ -1,32 +1,61 @@
 pub mod solutions {
   pub fn find_rearrangement_message(puzzle_input: &Vec<&str>) {
-    let initial_stacks = parse_initial_stacks(puzzle_input);
+    let mut stacks = parse_initial_stacks(puzzle_input);
+    let starting_index = calculate_stack_height(puzzle_input) + 2;
+
+    println!("Starting index: {}", puzzle_input[starting_index]);
+    for instruction_index in starting_index..puzzle_input.len() {
+      let instructions: Vec<&str> = puzzle_input[instruction_index]
+        .trim()
+        .split(' ')
+        .collect();
+      println!("Instruction: {instructions:?}");
+      println!("Len:         {}", instructions.len());
+      let amount = instructions[1].parse::<usize>().unwrap();
+      let source = instructions[3].parse::<usize>().unwrap() - 1;
+      let target = instructions[5].parse::<usize>().unwrap() - 1;
+      for _ in 0..amount {
+        let data = stacks[source].pop().unwrap();
+        stacks[target].push(data);
+      }
+      println!("Stacks Updated: {:?}", stacks)
+    }
+
+    let mut secret_message = "".to_owned();
+    stacks.iter().for_each(|s| secret_message.push_str(s.last().unwrap()));
+
+    println!("Secret message decrypted: {secret_message}");
+
   }
 
-  fn parse_initial_stacks(input: &Vec<&str>) {
-    let mut max_stack_height = 0;
-    for i in 0..input.len() {
-      if input[i].len() == 1 { 
-        max_stack_height = i - 1;
-        break;
+  fn parse_initial_stacks(input: &Vec<&str>) -> Vec<Vec<String>>{
+    let max_stack_height = calculate_stack_height(&input);
+
+    // Initialize stacks
+    let total_stacks = input[0].len() / 4;
+    let mut stacks: Vec<Vec<String>> = vec![];
+    for _ in 0..total_stacks { stacks.push(vec![]); }
+
+    // Populate stacks
+    for i in (0..max_stack_height).rev() {
+      let cleaned_string = input[i].replace(&['\r'][..], "");
+      let mut j = 1;
+      while j < cleaned_string.len() {
+        let ch = cleaned_string.get(j..j+1).unwrap();
+        if String::from(ch) != String::from(' ') { stacks[(j-1)/4].push(String::from(ch)); }
+        j += 4;
       }
     }
 
-    let total_stacks = input[0].len() / 4;
-    let mut stacks: Vec<Vec<&str>> = vec![];
-    for _i in 0..total_stacks { stacks.push(vec![]); }
+    println!("Initial Stacks: {stacks:?}");
+    return stacks;
+  }
 
-    println!("Stacks: {stacks:?}");
-
-    for i in (0..max_stack_height).rev() {
-      let row = input[i].replace(&['[',']'][..], "");
-      let split: Vec<String> = row.split(' ').map(|s| String::from(s)).collect();
-      split.iter()
-        .enumerate()
-        .for_each(|(i, entry)| stacks[i].push(&entry));
+  fn calculate_stack_height(input: &Vec<&str>) -> usize {
+    let mut max_stack_height = 0;
+    for i in 0..input.len() {
+      if input[i].len() == 1 { return i - 1 }
     }
-
-    println!("Stacks: {stacks:?}");
-
+    panic!("Stack height not found")
   }
 }
