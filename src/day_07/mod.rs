69dd9c22b96e48c866ac3e37837084b1773238c1
Justin Vaughn <justin@jvaughn.org>
2022-12-08 19:07:35 -0700

    Still working Day 7 Part 1, incomplete

--- src/day_07/mod.rs
+++ src/day_07/mod.rs
@@ -1,6 +1,20 @@
 #[allow(unused)]
 pub mod solutions {
-  use trees::Node;
+  use std::collections::HashMap;
+
+  #[derive(Debug, Clone)]
+  struct File {
+    name: String,
+    size: u32,
+  }
+
+  #[derive(Debug, Clone)]
+  struct Directory<'a> {
+    parent: &'a String,
+    name: String,
+    dirs: Vec<String>, // stores unique names of dirs instead of refs
+    files: Vec<File>,
+  }
 
   /// Day 7 Part 1 -- https://adventofcode.com/2022/day/7
   /// 
@@ -23,38 +37,60 @@ pub mod solutions {
   /// 14848514 b.txt   <-- This means there's a file here named "b.txt" with size 14848514
   /// ```
   pub fn find_sum_dirs_under_100000(cli_dump: &Vec<&str>) {
-    let root = Node { 
-      parent: null,
-      children: vec![], 
-      files: vec![] };
+    let mut directory: HashMap<String, Box<Directory>> = HashMap::new();
+    let mut current: Box<String> = Box::from("/".to_owned());
 
-    let mut current: &Node = &root;
+    let mut root_dir = Directory {
+      parent: &"".to_owned(),
+      name: "/".to_owned(),
+      dirs: vec![],
+      files: vec![],
+    };
+
+    directory.insert(String::from("/"), Box::from(root_dir));
+
     for line in cli_dump {
       let head = &line[0..4];
       let command: Vec<&str> = line.split(" ").collect();
+      let active_directory = String::from(&current.to_string());
       
-
       match head {
         "$ cd" => {
           println!("{}: change dir to {}", head, command[2]);
-          if command[2] == "/" { current = &root; }
-          else {
-            let new_node = Node {
-              
-            }
-          }
+          current = Box::from(command[2].to_owned());
+          continue;
         },
         "$ ls" =>  {
-          println!("{}: listing dir", head)
+          println!("{}: listing dir", head);
+          continue;
         },
         "dir " => {
-          println!("{}: new dir named '{}'", head, command[1])
+          println!("{}: new dir named '{}'", head, command[1]);
+          let mut current_dir = directory.get(&active_directory).unwrap().clone();
+          let new_dir = Directory {
+            parent: &current,
+            name: command[1].to_owned(),
+            dirs: vec![],
+            files: vec![],
+          };
+
+          current_dir.dirs.push(command[1].to_owned());
+          directory.insert(String::from(&current_dir.name), current_dir);
+          directory.insert(String::from(&new_dir.name), Box::from(new_dir));
+          continue;
         },
         _ => {
-          println!("{}: new file named {} of size {}", head, command[1], command[0])
+          println!("{}: new file named {} of size {}", head, command[1], command[0]);
+          let mut current_dir = directory.get(&active_directory).unwrap().clone();
+          let new_file = File {
+            name: command[1].to_owned(),
+            size: command[0].parse::<u32>().unwrap(),
+          };
+          current_dir.files.push(new_file);
+          directory.insert(active_directory, current_dir);
+          continue;
         },
       }
-      // println!("Split command: {:?}", command);
     }
   }
 }
